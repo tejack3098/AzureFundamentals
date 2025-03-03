a2405a3187b11ba5dbafe093173f8db5e9272f89
Tejas Choudhari <tejas.choudhari36@gmail.com>
2025-03-03 12:01:19 -0500

    Add project files.

---
+++ AzureBlobProject/Services/ContainerService.cs
@@ -0,0 +1,36 @@
+﻿
+using Azure.Storage.Blobs;
+using System.Runtime.CompilerServices;
+
+namespace AzureBlobProject.Services
+{
+    public class ContainerService : IContainerService
+    {
+        private readonly BlobServiceClient _blobClient;
+
+        public ContainerService(BlobServiceClient bobClient)
+        {
+               _blobClient = bobClient;
+        }
+
+        public Task CreateContainer(string containerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteContainer(string containerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<string>> GetAllContainer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<string>> GetAllContainerAndBlobs()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
